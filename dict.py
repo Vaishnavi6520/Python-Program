375682c362e14ca52a869323144aa9db3e3adfbd
Vaishnavi Hajela <65701834+Vaishnavi6520@users.noreply.github.com>
2021-07-21 12:10:03 +0530

    dictionary

---
+++ dict.py
@@ -0,0 +1,8 @@
+students={"Name":"Vaishnavi","age":"21","company":"Prodapt"}
+#students["age"]=20  #update
+#del students["age"] #delete
+# print(students)
+# students.clear()
+# print(students)
+# print(students.keys())
+# print(students.values())
---
+++ listmembership.py
@@ -0,0 +1,6 @@
+list=["Priya","Aditi","Vaidhi","Raja",23,23.3]
+name="23"
+if(name in list):
+    print("She is present")
+else:
+    print("She is absent")
