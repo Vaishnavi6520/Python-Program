5f23edf46026ee5c5fa8504fba18c2cbccc0d8a0
Vaishnavi Hajela <65701834+Vaishnavi6520@users.noreply.github.com>
2021-07-21 09:56:02 +0530

    Employee

---
+++ employee.py
@@ -0,0 +1,14 @@
+Employee=["Vaishnavi","Priya","Manvi","Vaidhi","Aparna","Aadrika","Sindhu"]
+state=["M.P","AP","U.P."]
+print(len(Employee)) #Finding the length of list
+print(Employee+state) # list concatenation
+
+print(state*4)
+#del Employee[3]
+#Employee.remove("Manvi")
+#print(Employee)
+#Employee.pop(1)
+#print(Employee)
+#Employee.clear()
+#Employee[0]="Aditi" #updating the list
+#print(Employee) #print all the items in the list
